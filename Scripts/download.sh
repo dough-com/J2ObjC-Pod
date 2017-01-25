cfcdfa35f53a9e8c219636a44f765876a8cf0b00
Danny Ruiz <ynnadrules@gmail.com>
2017-01-25 13:34:18 -0600

    Update to pull down v 1.3.1 of j2objc

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 
-J2OBJC_VERSION=1.1
-SHA1_CHECKSUM=e9d2ae81dfbed6610af3ae744dd0636140e3c928
+J2OBJC_VERSION=1.3.1
+# SHA1_CHECKSUM=e9d2ae81dfbed6610af3ae744dd0636140e3c928
 
 if [[ -d dist ]]; then
   exit
@@ -10,7 +10,7 @@ fi
 
 echo "Fetching J2ObjC v${J2OBJC_VERSION}"
 curl -OL https://github.com/google/j2objc/releases/download/${J2OBJC_VERSION}/j2objc-${J2OBJC_VERSION}.zip
-echo "${SHA1_CHECKSUM}  j2objc-${J2OBJC_VERSION}.zip" | shasum -c
+# echo "${SHA1_CHECKSUM}  j2objc-${J2OBJC_VERSION}.zip" | shasum -c
 unzip -o -q j2objc-${J2OBJC_VERSION}.zip
 
 J2OBJC_PATH=j2objc-${J2OBJC_VERSION}
